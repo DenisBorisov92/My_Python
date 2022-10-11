c50cfc41d32696c497f30a9b5d26d7897948f4a0
Denis <disa.92@list.ru>
2022-10-11 15:01:52 +0700

    поработали на лекции

---
+++ Hello Python(Lecture)/lecture_1.py
@@ -0,0 +1,20 @@
+# from base64 import b32decode
+
+
+# print('hello world')
+
+
+# print(a, '-',b, c)
+# a = int()
+# b = int()
+# c = None
+# print('{} + {} = {}'.format(a, b, c))
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print (a)
+# else: 
+#     print(b)
+r = range(10)
+for i in r:
+    print(i)
