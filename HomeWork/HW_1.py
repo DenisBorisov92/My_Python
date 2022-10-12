dcae452db06ec64141995a89e36a5e83f8b0d3e0
Denis <disa.92@list.ru>
2022-10-12 23:57:27 +0700

    tasks 4 and 5 are added

--- HomeWork/HW_1.py
+++ HomeWork/HW_1.py
@@ -20,15 +20,44 @@
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 
-X = float(input('Enter coordinate x: '))
-Y = float(input('Enter coordinate y: '))
-if X > 0 and Y > 0:
-    print('it is I quarter')
-if X < 0 and Y > 0:
-    print('it is II quarter')
-if X < 0 and Y > 0:
-    print('it is III quarter')
-if X > 0 and Y < 0:
-    print('it is IV quarter')
-else:
-    print('it is not correct coordinate')
+# X = float(input('Enter coordinate x: '))
+# Y = float(input('Enter coordinate y: '))
+# if X > 0 and Y > 0:
+#     print('it is I quarter')
+# if X < 0 and Y > 0:
+#     print('it is II quarter')
+# if X < 0 and Y > 0:
+#     print('it is III quarter')
+# if X > 0 and Y < 0:
+#     print('it is IV quarter')
+# else:
+#     print('it is not correct coordinate')
+
+#     Задача №4
+# Напишите программу, которая по заданному номеру четверти, 
+# показывает диапазон возможных координат точек в этой четверти (x и y).
+
+# quarter = input('Enter number of quater (from 1 to 4): ')
+# if quarter == 1:
+#     print('Х > 0 and Y > 0.')
+# elif quarter == 2:
+#     print('Х < 0 and Y > 0.')
+# elif quarter == 3:
+#     print('Х < 0 and Y < 0.')
+# elif quarter == 4:
+#     print('Х > 0 and Y < 0.')
+# else:
+#     print('Warning. It is not number of quarter')
+
+#     Задача №5
+# Напишите программу, которая принимает на вход координаты двух точек и 
+# находит расстояние между ними в 2D пространстве.
+# A (3,6); B (2,1) -> 5,09
+# A (7,-5); B (1,-1) -> 7,21
+
+Xa = float(input('Enter coordinate x of point A: '))
+Ya = float(input('Enter coordinate y of point A: '))
+Xb = float(input('Enter coordinate x of point B: '))
+Yb = float(input('Enter coordinate y of point B: '))
+distance = (((Xb - Xa)*2)+((Yb-Ya)*2))**0.5
+print(f'Distance between points A and B is {distance:.2f}')
