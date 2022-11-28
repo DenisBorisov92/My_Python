5cdd65ef1ab6a71a9bec6d2950f1da0b4af75949
Denis <disa.92@list.ru>
2022-11-28 14:10:50 +0700

    hw added

---
+++ HomeWork/HW_7/logger.py
@@ -0,0 +1,8 @@
+import interface
+from datetime import datetime
+
+
+def logger(information):
+    time=datetime.now().strftime('%H:%M')
+    with open('log.csv', 'a', encoding = "utf-8") as file:
+        file.write("{} {} \n". format(time, information))
